403fee86295a3ac8341e8bf4a0418f4ca138979c
Jaimie Lee <barnes.jaimielee@outlook.com>
2020-06-15 16:32:17 +0800

    add blog entry and links

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,15 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// Modal
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", function() {
+  modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function() {
+  modal.classList.remove("open-modal");
+});
